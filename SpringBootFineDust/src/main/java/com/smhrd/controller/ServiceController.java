0ebb93f124b2228724edeb53af09b67671678e0f
kimhozzi <kjho9808@gmail.com>
2025-05-30 17:13:43 +0900

    mainServ,chartTest

--- SpringBootFineDust/src/main/java/com/smhrd/controller/ServiceController.java
+++ SpringBootFineDust/src/main/java/com/smhrd/controller/ServiceController.java
@@ -19,4 +19,12 @@ public class ServiceController {
         }
 		return "service/serviceMain";
 	}
+
+
+
+
+
+
 }
+
+
---
+++ SpringBootFineDust/src/main/java/com/smhrd/controller/chartTestController.java
@@ -0,0 +1,17 @@
+package com.smhrd.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class chartTestController {
+
+
+
+	    @RequestMapping("/chartTest")
+	    public String showChart() {
+	        return "chartTest"; // 차트가 잘작동하는지 테스트한겁니다. 맘껏 지우셔도 됩니다 감사합니다.
+	    
+}
+}
