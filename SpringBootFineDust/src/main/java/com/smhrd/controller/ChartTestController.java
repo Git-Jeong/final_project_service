77d860052298f2f024452938824f5efc8a4bb85f
JeongJin <jeongjin9427@gmail.com>
2025-06-02 23:16:38 +0900

    Update : time System

--- SpringBootFineDust/src/main/java/com/smhrd/controller/ChartTestController.java
+++ SpringBootFineDust/src/main/java/com/smhrd/controller/ChartTestController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-public class chartTestController {
-
-
-
+public class ChartTestController {
+	
 	    @RequestMapping("/chartTest")
 	    public String showChart() {
 	        return "chartTest"; // 차트가 잘작동하는지 테스트한겁니다. 맘껏 지우셔도 됩니다 감사합니다.
