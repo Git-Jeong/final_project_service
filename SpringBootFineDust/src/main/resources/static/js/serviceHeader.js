b9e944b77287ab59fb341ab45c7870f4390595cb
JeongJin <jeongjin9427@gmail.com>
2025-06-06 11:27:34 +0900

    서비스 페이지 구조변경
    JS 통합 및 CSS 통합

--- SpringBootFineDust/src/main/resources/static/js/serviceHeader.js
+++ SpringBootFineDust/src/main/resources/static/js/serviceHeader.js
@@ -67,3 +67,23 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}, 1000);
 });
+
+const goLogout = () => {
+	if (confirm("로그아웃을 하시겠습니까?")) {
+		fetch("/logout", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+		.then(res => res.text())
+		.then(response => {
+			if (response === "success") {
+				location.href = "/";
+			}
+		})
+		.catch(error => {
+			alert("서버 오류: " + error);
+		});
+	}
+}
