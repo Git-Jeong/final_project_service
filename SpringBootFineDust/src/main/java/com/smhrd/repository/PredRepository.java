534e9f1b439d5bd8afabfdac9f16ec4d8208f39e
JeongJin <jeongjin9427@gmail.com>
2025-06-13 15:11:09 +0900

    Flask Controller

--- SpringBootFineDust/src/main/java/com/smhrd/repository/PredRepository.java
+++ SpringBootFineDust/src/main/java/com/smhrd/repository/PredRepository.java
@@ -1,15 +1,13 @@
 package com.smhrd.repository;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.smhrd.entity.Pred;
 
 public interface PredRepository extends JpaRepository<Pred, Integer> {
-	
-	ArrayList<Pred> findTop10ByStIdAndPmTimeLessThanEqualOrderByPmTimeDesc(int stId, LocalDateTime pmTime);
 
-	ArrayList<Pred> findTop10ByStIdAndPmTimeGreaterThanEqualOrderByPmTimeAsc(int stId, LocalDateTime pmTime);
+	List<Pred> findByStIdAndPmTimeAfterOrderByPmTimeDesc(int stId, LocalDateTime timeThreshold);
 
 }
