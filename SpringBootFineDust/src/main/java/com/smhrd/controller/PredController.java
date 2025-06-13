05adbe56002887a4580df64cda1082eb72eda956
kimhozzi <kjho9808@gmail.com>
2025-06-13 16:15:15 +0900

    Update PredController.java

--- SpringBootFineDust/src/main/java/com/smhrd/controller/PredController.java
+++ SpringBootFineDust/src/main/java/com/smhrd/controller/PredController.java
@@ -1,6 +1,5 @@
 package com.smhrd.controller;
 
-import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
